2cbf4d47d54c388a84a0eca6da45ee4051a4fd19
Ashok_Kumar <routashok04@gmail.com>
2021-08-01 12:59:49 +0530

    first commit

---
+++ src/main/java/com/example/CicdExample1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdExample1Application.class, args);
+	}
+
+}
