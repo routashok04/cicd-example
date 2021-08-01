0aef8dcbdf3910490279a2233b8e984c56b7dbf0
routashok04 <routashok04@gmail.com>
2021-08-01 13:23:00 +0530

    second commit

--- src/test/java/com/example/CicdExample1ApplicationTests.java
+++ src/test/java/com/example/CicdExample1ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CicdExample1ApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(CicdExample1Application.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Testcase executed...");
+		assertEquals(true, true);
 	}
 
 }
